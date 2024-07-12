a9f55f3b84ef9de108bdf2f40fe588027a1c0a12
nico <nicolasvo95@gmail.com>
2024-07-12 03:07:34 +0200

    upload output to minio

--- app.py
+++ app.py
@@ -1,53 +1,55 @@
 from fastapi import FastAPI, File, UploadFile, Form
 import demucs.separate
-# from minio import Minio
-# from minio.error import S3Error
+from minio import Minio
+from minio.error import S3Error
 
 import uuid
 import os
+from dotenv import load_dotenv
 
+load_dotenv()
 app = FastAPI()
 
 # TODO: default remove voice from song
 
 
-# def upload_minio(file_path: str, file_name: str, uuid: str):
+def upload_minio(file_path: str, file_name: str, uid: str):
+    client = Minio(
+        os.getenv("ENDPOINT_URL"),
+        access_key=os.getenv("ACCESS_KEY"),
+        secret_key=os.getenv("SECRET_KEY"),
+        secure=False,
+    )
 
-#     client = Minio(
-#         os.getenv("ENDPOINT_URL"),
-#         access_key=os.getenv("ACCESS_KEY"),
-#         secret_key=os.getenv("SECRET_KEY"),
-#         secure=False,
-#     )
+    # The file to upload, change this path if needed
+    source_file = file_path
 
-#     # The file to upload, change this path if needed
-#     source_file = file_path
+    # The destination bucket and filename on the MinIO server
+    bucket_name = "separated"
+    # destination_file = file_name
+    destination_file = f"{uid}/{file_name}"
 
-#     # The destination bucket and filename on the MinIO server
-#     bucket_name = "separated"
-#     destination_file = f""
+    # Make the bucket if it doesn't exist.
+    found = client.bucket_exists(bucket_name)
+    if not found:
+        client.make_bucket(bucket_name)
+        print("Created bucket", bucket_name)
+    else:
+        print("Bucket", bucket_name, "already exists")
 
-#     # Make the bucket if it doesn't exist.
-#     found = client.bucket_exists(bucket_name)
-#     if not found:
-#         client.make_bucket(bucket_name)
-#         print("Created bucket", bucket_name)
-#     else:
-#         print("Bucket", bucket_name, "already exists")
-
-#     # Upload the file, renaming it in the process
-#     client.fput_object(
-#         bucket_name,
-#         destination_file,
-#         source_file,
-#     )
-#     print(
-#         source_file,
-#         "successfully uploaded as object",
-#         destination_file,
-#         "to bucket",
-#         bucket_name,
-#     )
+    # Upload the file, renaming it in the process
+    client.fput_object(
+        bucket_name,
+        destination_file,
+        source_file,
+    )
+    print(
+        source_file,
+        "successfully uploaded as object",
+        destination_file,
+        "to bucket",
+        bucket_name,
+    )
 
 
 @app.post("/upload/")
@@ -58,10 +60,10 @@ async def upload_mp3_file(
 ):
     if file.content_type == "audio/mpeg":
         uid = str(uuid.uuid4())
-        tmp_dir = f"/tmp/{uid}/"
-        tmp_file = tmp_dir + file.filename
-        path = os.path.join("/tmp", uid)
-        os.mkdir(path)
+        tmp_dir = f"/tmp/{uid}"
+        tmp_file = tmp_dir + "/" + file.filename
+        track_name = file.filename.split(".mp3")[0]
+        os.makedirs(tmp_dir, exist_ok=True)
         with open(tmp_file, "wb") as f:
             f.write(file.file.read())
 
@@ -69,10 +71,14 @@ async def upload_mp3_file(
             demucs.separate.main(
                 ["--mp3", "--two-stems", track, tmp_file, "-o", f"/tmp/{uid}"]
             )
-            # /tmp/22288c4e-7184-4e54-8d1a-bcf0dd686592/htdemucs/numb
         elif separate == "everything":
             demucs.separate.main(["--mp3", tmp_file, "-o", f"/tmp/{uid}"])
-            # /tmp/7190ca17-316d-468c-8f38-44ea7df37ab2/htdemucs/numb
+
+        output_files = os.listdir(f"{tmp_dir}/htdemucs/{track_name}")
+        for f in output_files:
+            upload_minio(
+                f"{tmp_dir}/htdemucs/{track_name}/{f}", f"{track_name}_{f}", uid
+            )
 
         return {
             "filename": tmp_file,
